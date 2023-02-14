7f2bcbb4b167bf5c52429c0a20fed98d76973a9b
N.V.Suresh Krishna <62717096+sureshkrishna123@users.noreply.github.com>
2023-02-14 13:14:13 +0530

    Update indian_sign.py

--- indian_sign.py
+++ indian_sign.py
@@ -84,7 +84,7 @@ if app_mode=='Image to Text':
   if (button_translate and image_file) or (button_translate and image_file)  :
     class_names= ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     img_height,img_width=180,180
-    model = load_model('indian_sign.h5')
+    model = load_model('keras_model.h5')
     #class_names = model.class_names
     demo_image_path = image_file
     img = tf.keras.utils.load_img(demo_image_path, target_size=(img_height, img_width))
